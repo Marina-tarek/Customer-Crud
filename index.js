dbe09458df82441acfcd7a8390b884c36b33f58b
Marina-tarek <marina.tarek.w@gmail.com>
2025-07-19 02:51:43 +0300

    fourth commit

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ content1.innerHTML = `
 <label for="Lname"> last Name : </label><input type="text" id="Lname">
 <label for="age">Age : </label> <input type="number" id="age"> 
 <button class="btn-add">Add <i class="fa-solid fa-user-plus"></i></button>
-<button onclick="saveUpdating()"id="saveUpdate" class="display-none">SaveUpdate <i class="fa-solid fa-floppy-disk"></i></button> 
 </div>
 </div>`
 document.body.appendChild(content1)
@@ -23,7 +22,6 @@ var lastName = document.getElementById("Lname")
 var Age = document.getElementById("age")
 var btnAdd = document.querySelector(".btn-add")
 var btnDelete = document.querySelector(".btn-delete")
-
 var btnSearch = document.querySelector(".btn-search")
 var saveUpdate = document.querySelector("#saveUpdate")
 var searchInput = document.querySelector(".search-input")
@@ -40,22 +38,25 @@ if (localStorage.getItem("customer") == null) {
 }
 
 function addCustomer() {
-
     var people = {
         fname: firstName.value,
         lname: lastName.value,
         age: Age.value,
         isEdit: false
     }
-    console.log(firstName.value);
 
     if ((firstName.value != "") && (lastName.value != "") && (Age.value != "")) {
-        console.log(people);
-        custmerList.push(people)
-        console.log(custmerList);
-        clearInputs()
-        showCstomer()
-        localStorage.setItem("customer", JSON.stringify(custmerList))
+
+        if (validateAge(Age.value) == false || validateName(firstName.value) == false || validateName(lastName.value) == false) {
+            alert("please enter your name and age correct")
+        } else {
+            custmerList.push(people)
+            console.log(custmerList);
+            clearInputs()
+            showCstomer()
+            localStorage.setItem("customer", JSON.stringify(custmerList))
+        }
+
     } else {
         alert("you Must fill all information detalies")
     }
@@ -71,9 +72,9 @@ function clearInputs() {
 function showCstomer() {
     var cartona = '';
     for (let i = 0; i < custmerList.length; i++) {
-        cartona += ` <tr><td id="one" class="info"><div>${custmerList[i].fname}</div><input id="f${i}" type="text" value="${custmerList[i].fname}" class="test display-none"></td>
-<td id="two" class="info"><div>${custmerList[i].lname}</div><input id="l${i}" type="text" value="${custmerList[i].lname}" class="test display-none"></td>
-<td id="three" class="info"><div>${custmerList[i].age}</div><input id="a${i}"  type="text" value="${custmerList[i].age}" class="test display-none"></td>
+        cartona += ` <tr><td id="one" class="info"><div>${custmerList[i].fname}</div><input type="text" value="${custmerList[i].fname}" class="test display-none"></td>
+<td id="two" class="info"><div>${custmerList[i].lname}</div><input type="text" value="${custmerList[i].lname}" class="test display-none"></td>
+<td id="three" class="info"><div>${custmerList[i].age}</div><input type="text" value="${custmerList[i].age}" class="test display-none"></td>
 <td class="text-center"><button onclick="editInformation(${i})" id="btn-edit">Edit <i class="fa-solid fa-pen-to-square"></i></button>
 <button onclick="saveupdating(${i})"id="btn-save" class="display-none">Save <i class="fa-solid fa-floppy-disk"></i></button>
 <button class="btn-delete" onclick="deleteCustomer(${i})">Delete<i class="fa-solid fa-trash"></i></button></td>
@@ -95,11 +96,11 @@ function searchCustomer() {
     console.log(searchInput.value);
     cartona = ``
     for (let i = 0; i < custmerList.length; i++) {
-        if (custmerList[i].fname.toLowerCase().includes(term.toLowerCase()) == true || custmerList[i].lname.toLowerCase().includes(term.toLowerCase()) == true) {
+        if (custmerList[i].fname.toLowerCase().includes(term.toLowerCase()) == true && custmerList[i].lname.toLowerCase().includes(term.toLowerCase()) == true) {
             cartona += ` <tr>
-<td class="info">${custmerList[i].fname}</td>
-<td class="info">${custmerList[i].lname}</td>
-<td class="info">${custmerList[i].age}</td>
+<td class="info"><div>${custmerList[i].fname}</div><input type="text" value="${custmerList[i].fname}" class="test display-none"></td>
+<td class="info"><div>${custmerList[i].lname}</div><input type="text" value="${custmerList[i].lname}" class="test display-none"></td>
+<td class="info"><div>${custmerList[i].age}</div><input  type="text" value="${custmerList[i].age}" class="test display-none"></td>
 <td class="text-center"><button onclick="editInformation(${i})" id="btn-edit">Edit <i class="fa-solid fa-pen-to-square"></i></button>
 <button onclick="saveUpdating(${i})" id="btn-save" class="display-none">Save <i class="fa-solid fa-floppy-disk"></i></button>
 <button class="btn-delete" onclick="deleteCustomer(${i})">Delete <i class="fa-solid fa-trash"></i></button></td>
@@ -116,6 +117,7 @@ function searchCustomer() {
         custmerList = JSON.parse(localStorage.getItem("customer"))
         showCstomer()
     }
+    
     if (isValid == false) {
         alert("Not found in First name and last name ")
         showCstomer()
@@ -132,28 +134,28 @@ function clearSearch() {
 function editInformation(x) {
     ///////// updateIndex=x;
     var btnEdit = document.querySelectorAll("#btn-edit")[x]
-var btnSave = document.querySelectorAll("#btn-save")[x]
- btnEdit.classList.add("display-none")
- btnSave.classList.remove("display-none")
- var row = document.querySelectorAll("tbody tr")[x]
-var ar=[0,1,2]
-var tabledesc;
-var tableInput;
-for (let i = 0; i < ar.length; i++) {
-tabledesc=row.querySelectorAll("div")[i]
-console.log(tabledesc);
- tableInput=tabledesc.nextElementSibling
-console.log(tableInput);
-    tabledesc.classList.add("display-none")
-    tableInput.classList.remove("display-none")
-}
+    var btnSave = document.querySelectorAll("#btn-save")[x]
+    btnEdit.classList.add("display-none")
+    btnSave.classList.remove("display-none")
+    var row = document.querySelectorAll("tbody tr")[x]
+    var ar = [0, 1, 2]
+    var tabledesc;
+    var tableInput;
+    for (let i = 0; i < ar.length; i++) {
+        tabledesc = row.querySelectorAll("div")[i]
+        console.log(tabledesc);
+        tableInput = tabledesc.nextElementSibling
+        console.log(tableInput);
+        tabledesc.classList.add("display-none")
+        tableInput.classList.remove("display-none")
+    }
 
     //////////////// // firstName.value = custmerList[x].fname
     //////////////// lastName.value = custmerList[x].lname
     //////////// Age.value = custmerList[x].age
-       ////btnAdd.classList.add("display-none")
+    ////btnAdd.classList.add("display-none")
     /////////saveUpdate.classList.remove("display-none")
-    
+
     //     for (let i = 0; i < custmerList.length; i++) {
     //     var input=document.getElementsByTagName("td")[btn]
 
@@ -169,40 +171,40 @@ console.log(tableInput);
 
 function saveupdating(x) {
     var btnEdit = document.querySelectorAll("#btn-edit")[x]
-var btnSave = document.querySelectorAll("#btn-save")[x]
- btnEdit.classList.remove("display-none")
- btnSave.classList.add("display-none")
+    var btnSave = document.querySelectorAll("#btn-save")[x]
+    btnEdit.classList.remove("display-none")
+    btnSave.classList.add("display-none")
 
-  var row = document.querySelectorAll("tbody tr")[x]
-var ar=[0,1,2]
-var tabledesc;
-var tableInput;
-for (let i = 0; i < ar.length; i++) {
-tabledesc=row.querySelectorAll("div")[i]
-console.log(tabledesc);
- tableInput=tabledesc.nextElementSibling
-console.log(tableInput);
-    tabledesc.classList.remove("display-none")
-    tableInput.classList.add("display-none")
+    var row = document.querySelectorAll("tbody tr")[x]
+    var ar = [0, 1, 2]
+    var tabledesc;
+    var tableInput;
+    for (let i = 0; i < ar.length; i++) {
+        tabledesc = row.querySelectorAll("div")[i]
+        console.log(tabledesc);
+        tableInput = tabledesc.nextElementSibling
+        console.log(tableInput);
+        tabledesc.classList.remove("display-none")
+        tableInput.classList.add("display-none")
 
-    console.log(row.querySelectorAll(".test")[i].value);
+        console.log(row.querySelectorAll(".test")[i].value);
 
-}
+    }
     custmerList[x].fname = row.querySelectorAll(".test")[0].value
-   custmerList[x].lname = row.querySelectorAll(".test")[1].value
+    custmerList[x].lname = row.querySelectorAll(".test")[1].value
     custmerList[x].age = row.querySelectorAll(".test")[2].value
-showCstomer()
+    showCstomer()
 
 
-   //////////////////////////////// console.log("hello");
-   /////////////////////////////// btnAdd.classList.remove("display-none")
+    //////////////////////////////// console.log("hello");
+    /////////////////////////////// btnAdd.classList.remove("display-none")
     ///////////////////////////saveUpdate.classList.add("display-none")
     ///////////////////custmerList[updateIndex].fname = firstName.value
-   ///////////////// custmerList[updateIndex].lname = lastName.value
+    ///////////////// custmerList[updateIndex].lname = lastName.value
     //////////////////////////custmerList[updateIndex].age = Age.value
     /////////////////////showCstomer()
     ///////////////////localStorage.setItem("customer", JSON.stringify(custmerList))
-   /////////////////// clearInputs()
+    /////////////////// clearInputs()
     // var btnEdit = document.querySelectorAll("#btn-edit")
     // var btnSave = document.querySelectorAll("#btn-save")
     // console.log(x);
@@ -211,12 +213,36 @@ showCstomer()
 
 
 
- 
+
 
 }
 
 
 
+function validateAge(ageInput) {
+    const ageRegex = /^(0?[1-9]|[1-9][0-9]|1[01][0-9]|120)$/;
+
+    if (ageRegex.test(ageInput)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validateName(nameInput) {
+    const nameRegex = /^[a-zA-Z\s'-]+$/;
+
+    if (nameRegex.test(nameInput)) {
+        return true;
+    } else {
+        return false;
+    }
+
+
+}
+
+
+//////<button onclick="saveUpdating()"id="saveUpdate" class="display-none">SaveUpdate <i class="fa-solid fa-floppy-disk"></i></button>
 
 ////------------------------------------------------
 // function updateInput() {
